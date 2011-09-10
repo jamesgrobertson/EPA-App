6dc5d376cba4d20754fb0c35f0a832493136732f
James Robertson <jamesrobertson@braverycorp.com>
2011-09-10 18:52:30 -0400

    Working forms

--- app.php
+++ app.php
@@ -1,19 +1,29 @@
-<?php
-require_once '../krumo/class.krumo.php';
-$client = new SoapClient('http://iaspub.epa.gov/WATERSWebServices/SpatialServices?WSDL', array('trace' => true, 'exceptions' => true));
-$args = array('latitude' => 32.654, 'longitude' => -79.939, 'searchRadiusMiles' => 10, 'programsList' => array('array' => array('303D')));
+<html>
+	<body>
+		<?php
+		require_once '../krumo/class.krumo.php';
+		if (!empty($_POST['latitude']) && !empty($_POST['longitude'])) {
+			krumo($_POST);
+		} elseif (!empty($_POST['street']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zip'])){
+			krumo($_POST);	
+		} else{ krumo($_POST);}
+		function soap($latitude, $longitude, $radius) {
+			$client = new SoapClient('http://iaspub.epa.gov/WATERSWebServices/SpatialServices?WSDL', array('trace' => true, 'exceptions' => true));
+			$args = array('latitude' => $latitude, 'longitude' => $longitude, 'searchRadiusMiles' => 10, 'programsList' => array('array' => array('303D')));
 
-try {
-	$stuff = $client -> __soapCall('getEntitiesByLatLong', $args);
-} catch (SoapFault $e) {
-	echo "SOAP Fault: " . $e -> getMessage() . "<br />\n";
-}
-
-//* DEBUG
-echo "<pre>\n\n";
-echo "Request :\n";
-echo htmlspecialchars($client -> __getLastRequest()) . "\n";
-echo "</pre>";
-//*/
-krumo($client->__getTypes());
-?>
+			try {
+				$stuff = $client -> __soapCall('getEntitiesByLatLong', $args);
+			} catch (SoapFault $e) {
+				echo "SOAP Fault: " . $e -> getMessage() . "<br />\n";
+			}
+			//* DEBUG
+			echo "<pre>\n\n";
+			echo "Request :\n";
+			echo htmlspecialchars($client -> __getLastRequest()) . "\n";
+			echo "</pre>";
+			//*/
+			krumo($client -> __getTypes());
+		}
+		?>
+	</body>
+</html>
