3f64194efcef41181a8531066ffc4a8f22f1bcdd
James Robertson <jamesrobertson@braverycorp.com>
2011-09-11 16:49:26 -0400

    Code cleanup

--- app.php
+++ app.php
@@ -4,16 +4,7 @@
 		require_once '../krumo/class.krumo.php';
 		require_once 'lib/nusoap.php';
 		if (is_numeric($_POST['latitude']) && is_numeric($_POST['longitude']) && is_numeric($_POST['radius'])) {
-			$params = /*<SOAP-ENV:Envelope
-			 xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-			 xmlns:ns1="http://iaspub.epa.gov/WATERSWebServices/OWServices"
-			 xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-			 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-			 xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-			 >
-			 <SOAP-ENV:Body>*/
-			 //<ns1:getEntitiesByLatLong>
-			 '<latitude xsi:type="xsd:decimal">' . $_POST['latitude'] . '</latitude>
+			$params = '<latitude xsi:type="xsd:decimal">' . $_POST['latitude'] . '</latitude>
 			 <longitude xsi:type="xsd:decimal">' . $_POST['longitude'] . '</longitude>
 			 <searchRadiusMiles xsi:type="xsd:decimal">' . $_POST['radius'] . '</searchRadiusMiles>
 			 <programsList
@@ -30,14 +21,9 @@
 			 <string  xsi:type="xsd:string">303D</string>
 			 </array>
 			 </programsList>';
-			 //</ns1:getEntitiesByLatLong>';
-			 //</SOAP-ENV:Body>
-			 //</SOAP-ENV:Envelope>';
-			//$params = array('latitude' => $_POST['latitude'], 'longitude' => $_POST['longitude']);
-			$url = 'http://iaspub.epa.gov/WATERSWebServices/SpatialServices';
 			$wsdl = 'http://iaspub.epa.gov/WATERSWebServices/SpatialServices?WSDL';
 			$client = new nusoap_client($wsdl);
-			$results = $client -> call('getEntitiesByLatLong',$params);
+			$results = $client -> call('getEntitiesByLatLong', $params);
 			$error = $client -> getError();
 			krumo($results);
 			if ($error) { print $error;
