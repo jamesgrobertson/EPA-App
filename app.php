359f3cbd6fff19e84fc942e9703857e2229338be
James Robertson <jamesrobertson@braverycorp.com>
2011-09-11 14:29:01 -0400

    Removed address form

--- app.php
+++ app.php
@@ -2,17 +2,16 @@
 	<body>
 		<?php
 		require_once '../krumo/class.krumo.php';
-		if (!empty($_POST['latitude']) && !empty($_POST['longitude'])) {
-			krumo($_POST);
-		} elseif (!empty($_POST['street']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zip'])){
-			krumo($_POST);	
+		if (is_numeric($_POST['latitude']) && is_numeric($_POST['longitude']) && is_numeric($_POST['radius'])) {
+			$results = soap($_POST['latitude'], $_POST['longitude'], $_POST['radius']);
 		} else{ krumo($_POST);}
 		function soap($latitude, $longitude, $radius) {
 			$client = new SoapClient('http://iaspub.epa.gov/WATERSWebServices/SpatialServices?WSDL', array('trace' => true, 'exceptions' => true));
-			$args = array('latitude' => $latitude, 'longitude' => $longitude, 'searchRadiusMiles' => 10, 'programsList' => array('array' => array('303D')));
+			$args = array('latitude' => $latitude, 'longitude' => $longitude, 'searchRadiusMiles' => $radius, 'programsList' => array('array' => array('303D')));
 
 			try {
 				$stuff = $client -> __soapCall('getEntitiesByLatLong', $args);
+				return $stuff;
 			} catch (SoapFault $e) {
 				echo "SOAP Fault: " . $e -> getMessage() . "<br />\n";
 			}
